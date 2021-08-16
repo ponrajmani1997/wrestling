866f387048cd78471bfca4aa62e3c2e8af08b3d3
ponraj mani <ponrajmani1997@gmail.com>
2021-08-16 11:41:35 +0530

    first commit

---
+++ UNDERTAKER/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def UT(request):
+    return HttpResponse('CHOKE SLAM is the smack of undertaker')
